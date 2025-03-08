abb376adaf049ea53c69e74596c501b8b5eccd0c
pratham-soni <spratham304@gmail.com>
2025-03-08 21:12:46 +0530

    change sol

--- two-pointers/28. Find the Index of the First Occurrence in a String.py
+++ two-pointers/28. Find the Index of the First Occurrence in a String.py
@@ -23,13 +23,15 @@ class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         if needle =="":
             return 0
-        
-        # for i in range(len(haystack)+1-len(needle)):
-        #     for j in range(len(needle)):
-        #         if haystack[i+j] != needle[j]:
-        #             break
-        #         if j == len(needle)-1:
-        #             return i
+
+        """
+        for i in range(len(haystack)+1-len(needle)):
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    break
+                if j == len(needle)-1:
+                    return i
+        """
         
         for i in range(len(haystack)+1-len(needle)):
             if haystack[i: i+len(needle)] == needle:
